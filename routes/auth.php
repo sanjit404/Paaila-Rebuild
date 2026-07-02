b0dcc5438d62a267556d5a3f7c88e61a98db9862
sanjit404 <sanjitpoudel7@gmail.com>
2026-07-02 20:09:26 +0545

    Https and verification

--- routes/auth.php
+++ routes/auth.php
@@ -10,8 +10,6 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\URL;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
@@ -41,18 +39,9 @@ Route::middleware('auth')->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
         ->name('verification.notice');
 
-Route::get('/verify-email/{id}/{hash}', function (Request $request) {
-    dd([
-        'hasValidSignature' => URL::hasValidSignature($request),
-        'fullUrl' => $request->fullUrl(),
-        'url' => $request->url(),
-        'secure' => $request->isSecure(),
-        'host' => $request->getHost(),
-        'app_url' => config('app.url'),
-    ]);
-})
-->middleware('auth')
-->name('verification.verify');
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
 
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
         ->middleware('throttle:6,1')
